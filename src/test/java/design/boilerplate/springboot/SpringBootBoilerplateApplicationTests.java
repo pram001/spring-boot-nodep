438c4b1f3b6b899a3ef01dd15656f0786b5a0324
pram001 <praneeth1706manda@gmail.com>
2022-06-22 01:01:29 +0530

    first commit

---
+++ src/test/java/design/boilerplate/springboot/SpringBootBoilerplateApplicationTests.java
@@ -0,0 +1,13 @@
+package design.boilerplate.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootBoilerplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
